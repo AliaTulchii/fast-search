c3ca653543ad667a257b43528b6ca4d5125ea902
Halina Tulchii <halendeihalyna@gmail.com>
2024-08-21 12:52:25 +0300

    added all needed setup for react app and create the Home page

---
+++ src/components/JobListings.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import jobs from "../jobs.json";
+import Job from "./Job";
+
+const JobListings = () => {
+    console.log(jobs);
+    const recentJobs = jobs.slice(0, 3)
+
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-purple-500 mb-6 text-center">
+          Browse Jobs
+        </h2>
+        <ul className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {recentJobs.map((job) => (
+              <Job key={job.id} job={job} />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default JobListings;
