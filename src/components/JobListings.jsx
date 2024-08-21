22c59c7cd0f85dc2314f7a1ade1f24b4c564550b
Halina Tulchii <halendeihalyna@gmail.com>
2024-08-21 19:40:04 +0300

    changed main styles added new logo, Jobs Page

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -9,7 +9,7 @@ const JobListings = () => {
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-purple-500 mb-6 text-center">
+        <h2 className="text-3xl font-bold text-fuchsia-700 mb-6 text-center">
           Browse Jobs
         </h2>
         <ul className="grid grid-cols-1 md:grid-cols-3 gap-6">
